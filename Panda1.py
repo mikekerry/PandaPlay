56dd95264a74d626dd01cae53fc597a3b23fcf64
Michael Kerry <Michael Kerry>
2016-11-19 21:16:04 +0000

    converting time col to datetime

--- Panda1.py
+++ Panda1.py
@@ -1,11 +1,20 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
+from dateutil.parser import parse
 
 df = pd.read_table('C:\Git\PandaPlay\BurringtonCombeRide_tcx.txt', sep='\t')
 
 #summary pf dataframe
 df.info()
+
+#convert time column to datetime
+df['time'] = pd.to_datetime(df['time'])
+
+#view first 5 and last 5 values
+df.head()
+df.tail()
 
 #summary statistics
 df.describe()
