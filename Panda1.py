42f2274b8009f10d38e8f605c87f0cf033b21673
Michael Kerry <Michael Kerry>
2016-11-19 19:57:08 +0000

    adding corr() function

--- Panda1.py
+++ Panda1.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 
 df = pd.read_table('C:\Git\PandaPlay\BurringtonCombeRide_tcx.txt', sep='\t')
 
+#summary pf dataframe
 df.info()
 
+#summary statistics
 df.describe()
+
+#return a dataframe showing matrix of correlation
+df.corr()
+
+
+
